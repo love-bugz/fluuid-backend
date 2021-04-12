62a55aea629734e3f29ef4a9dfd42ef52d5aa700
kembreyfarquhar <kembreyfarquhar@gmail.com>
2021-04-12 11:04:23 -0500

    removes local host from app.listen

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ dotenv.config();
 
 const port: number = parseInt(process.env.PORT || '5000', 10);
 
-app.listen(port, '127.0.0.1', () => {
+app.listen(port, () => {
 	console.log(`*** SERVER LISTENING ON PORT:${port}\n`);
 });
