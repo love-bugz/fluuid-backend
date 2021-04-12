bea91061b2e6fe8e0952ffd9cc8358e010a7dab4
kembreyfarquhar <kembreyfarquhar@gmail.com>
2021-04-12 10:24:05 -0500

    changes db.ts config file

--- src/api/db.ts
+++ src/api/db.ts
@@ -1,18 +1,40 @@
 import { createConnection } from 'typeorm';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const connect = async () => {
-	const connection = await createConnection({
-		type: 'sqlite',
-		database: 'dev.db',
-		name: 'default',
-		synchronize: false,
-		logging: true,
-		migrationsTableName: 'custom_migration_table',
-		entities: [__dirname + '/models/*.model.ts'],
-		migrations: [__dirname + '/migrations/*.ts'],
-		cli: {
-			entitiesDir: __dirname + './models/',
-			migrationsDir: __dirname + './migrations/',
-		},
-	});
+	const db_url = process.env.DATABASE_URL || null;
+	if (db_url) {
+		const connection = await createConnection({
+			type: 'postgres',
+			synchronize: false,
+			logging: true,
+			url: db_url,
+			extra: {
+				ssl: true,
+			},
+			migrationsTableName: 'custom_migration_table',
+			entities: [__dirname + '/models/*.model.ts'],
+			migrations: [__dirname + '/migrations/*.ts'],
+			cli: {
+				entitiesDir: __dirname + './models/',
+				migrationsDir: __dirname + './migrations/',
+			},
+		});
+	} else {
+		const connection = await createConnection({
+			type: 'sqlite',
+			database: 'dev.db',
+			name: 'default',
+			synchronize: false,
+			logging: true,
+			migrationsTableName: 'custom_migration_table',
+			entities: [__dirname + '/models/*.model.ts'],
+			migrations: [__dirname + '/migrations/*.ts'],
+			cli: {
+				entitiesDir: __dirname + './models/',
+				migrationsDir: __dirname + './migrations/',
+			},
+		});
+	}
 };
